82f3c36ac1171420d8866718bb8f8c96599e7bec
Manit Cholpinyo <manit.cholpinyo@krungsri.com>
2020-01-23 16:07:43 +0700

    design tik tok ui

---
+++ 001-tik_tok_ui/phase2/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/localadm/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/localadm/Workspace/Spike/Flutter/flutter-tutorials/001-tik_tok_ui/phase2"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/localadm/Downloads/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
